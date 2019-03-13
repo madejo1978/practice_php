4b6deeace827d9559c77b28b1ffdac7e0a1e021b
Martin de Jonge <madejo@hotmail.com>
2019-03-13 16:09:24 +0100

    v1.8.7

--- website8/blog.php
+++ website8/blog.php
@@ -4,6 +4,29 @@ require ('config/db.php');
 // SINGLE BLOG-POST
 /////////////////////
 
+// Activate the Submit-button, so the data can be send to the database
+if(isset($_POST['delete'])){
+    // check with echo if the Delete-button works
+        // echo 'Halleluja the Delete-button works!';
+
+    // Delete the blog using the id inputted in the hidden-form (see below: "name") in a variable
+    $delete_id  = mysqli_real_escape_string($conn, $_POST['delete_id']);
+    
+    // Use the query to delete the blog from the database
+    $query = "DELETE FROM blogs WHERE id = '$delete_id'";
+    // var_dump($query);
+
+    // if connection with the database, then put the query in the database
+    if(mysqli_query($conn, $query)){
+        // if deleted, then redirect to index.php
+        header('Location:' . ROOT_URL);
+        } else {
+            // echo the error-code
+            echo '<br>' . 'Failed to delete this Blog from the Database' . '<br>'; 
+            echo 'MySQLI error code is' . ' ' . mysqli_error($conn); 
+    }  
+}
+
 // get unique id
 $id = mysqli_real_escape_string($conn, $_GET['id']); // Escapes special characters in a string for use in an SQL statement
 // echo $id;
@@ -32,9 +55,13 @@ $query = 'SELECT id, title, body, author,created_at FROM blogs WHERE ID= ' . $id
         <div class = "jumbotron">         
             <small>Created on <?php echo $blog['created_at']; ?> by
             <?php echo $blog['author']; ?></small>
-            <p><?php echo $blog['body']; ?></p>
-            <a class = "btn btn-outline-secondary" href="<?php echo constant("ROOT_URL"); ?>">Back to Blogs-index</a> <!-- root is specified in config/config.php -->
+            <p><?php echo $blog['body']; ?></p>            
             <a class = "btn btn-outline-secondary" href="<?php echo constant("ROOT_URL"); ?>editblog.php?id=<?php echo $blog['id']; ?>">Edit Blog</a> <!-- root is specified in config/config.php -->
+        
+            <form class="float-right" method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
+                <input type="hidden" name="delete_id" value="<?php echo $blog['id']; ?>">
+                <input type="submit" name="delete" value="Delete" class="btn btn-danger">
+            </form>
         </div>
     </div>
 
--- website8/index.php
+++ website8/index.php
@@ -33,5 +33,13 @@ $query = 'SELECT id, title, body, author,created_at FROM blogs ORDER BY created_
         <?php endforeach; ?>
     </div>
 
-<?php include('inc/footer.php') ?>
-
+    <script
+        src="http://code.jquery.com/jquery-1.12.4.js"
+        integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
+        crossorigin="anonymous"></script>
+    
+    <script 
+        src = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
+    </script>
+</body>
+</html>
