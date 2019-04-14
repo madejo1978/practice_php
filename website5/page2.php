e25b72b801d1b2fe815784b23084b020c2dec8b0
Martin <madejo@hotmail.com>
2019-04-14 11:01:19 +0200

    v2.3.2

--- website5/page2.php
+++ website5/page2.php
@@ -4,12 +4,19 @@
 setcookie('username', "Activate Cookie without using a 'Form'", time() + (86400 * 30) );
 
 // delete cookie
-setcookie('username', "Activate Cookie without using a 'Form'", time() - 3600);
+// setcookie('username', "Activate Cookie without using a 'Form'", time() - 3600);
+
+// check if there are cookies active
+if(count($_COOKIE) > 0){
+    echo 'There are ' . count($_COOKIE) . ' cookies active' . '<br>';
+} else {
+    echo 'There are no cookies active';
+}
 
 if(isset($_COOKIE['username'])){
-    echo 'Welcome ' . $_COOKIE['username'] . ' your cookie is active' . '<br>';
+    echo 'Welcome ' . $_COOKIE['username'] . '<br>' . ' Your cookie is active' . '<br>';
 } else {
-    echo 'The cooke is NOT active';
+    echo 'The cookie is NOT active';
 }
 
 ?>
